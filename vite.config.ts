674fa93bccfefa0a9adabb87a23380929bf98361
Gökçe Merdun <agmmnn@gmail.com>
2023-08-13 22:58:15 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,12 @@
-import { defineConfig } from "vite"
 import { resolve } from "node:path"
+import { defineConfig } from "vite"
 
 // https://vitejs.dev/config/
 export default defineConfig(async () => ({
   plugins: [],
   resolve: {
     alias: {
-      "@/": resolve(__dirname, "./src"),
+      "@": resolve(__dirname, "./src"),
     },
   },
   // Vite options tailored for Tauri development and only applied in `tauri dev` or `tauri build`
